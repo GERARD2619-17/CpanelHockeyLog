f35397b2aaa3c9d899eef6c7ea47d58246871185
GERARD2619-17 <gera121134@gmail.com>
2023-10-31 17:30:24 -0600

    .

--- logearme.js
+++ logearme.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const password = document.getElementById('password').value;
 
         try {
-            const response = await fetch('https://gerard2619-17.github.io/CpanelHockeyLog/localhost:1337/api/auth/local', {
+            const response = await fetch('https://localhost:1337/api/auth/local/gerard2619-17.github.io/CpanelHockeyLog', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
