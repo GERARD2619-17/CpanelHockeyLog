bb78c143a82b388206216e73f67701c306ab2695
GERARD2619-17 <gera121134@gmail.com>
2023-10-31 17:24:29 -0600

    prueba de logueo

--- logearme.js
+++ logearme.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const password = document.getElementById('password').value;
 
         try {
-            const response = await fetch('http://localhost:1337/api/auth/local', {
+            const response = await fetch('https://gerard2619-17.github.io/CpanelHockeyLog/localhost:1337/api/auth/local', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
